5413f580776a3a306d988174a6a33dd8ffdb31af
D.J. Figueroa <djfigs10@gmail.com>
2025-04-21 23:38:54 -0400

    init commit

---
+++ Editor/PropertyDrawers/CornerRadiiPropertyDrawer.cs
@@ -0,0 +1,59 @@
+using Tactile.Core.Extensions;
+using Tactile.Core.Utility;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tactile.UI.Editor.PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(Rectangle.CornerRadii))]
+    public class CornerRadiiPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var topLeft = property.FindPropertyRelative(nameof(Rectangle.CornerRadii.topLeft));
+            var topRight = property.FindPropertyRelative(nameof(Rectangle.CornerRadii.topRight));
+            var bottomLeft = property.FindPropertyRelative(nameof(Rectangle.CornerRadii.bottomLeft));
+            var bottomRight = property.FindPropertyRelative(nameof(Rectangle.CornerRadii.bottomRight));
+
+            position = EditorGUI.PrefixLabel(position, label);
+            
+            var rects = position.HorizontalLayout(RectLayout.SingleLineHeight,
+                RectLayout.Repeat(4, RectLayout.Flex()));
+
+            var topLeftName = $"{property.name}-topLeft";
+            var topRightName = $"{property.name}-topRight";
+            var bottomLeftName = $"{property.name}-bottomLeft";
+            var bottomRightName = $"{property.name}-bottomRight";
+
+            GUI.SetNextControlName(topLeftName);
+            EditorGUI.PropertyField(rects[1], topLeft, GUIContent.none);
+            
+            GUI.SetNextControlName(topRightName);
+            EditorGUI.PropertyField(rects[2], topRight, GUIContent.none);
+            
+            GUI.SetNextControlName(bottomLeftName);
+            EditorGUI.PropertyField(rects[3], bottomLeft, GUIContent.none);
+            
+            GUI.SetNextControlName(bottomRightName);
+            EditorGUI.PropertyField(rects[4], bottomRight, GUIContent.none);
+            
+            
+            var currentSelected = GUI.GetNameOfFocusedControl();
+            var borderRadiusImage = TactileEditorTextures.GetTexture(currentSelected switch
+            {
+                _ when currentSelected == topLeftName => TactileEditorTextures.BorderRadiusTopLeft,
+                _ when currentSelected == topRightName => TactileEditorTextures.BorderRadiusTopRight,
+                _ when currentSelected == bottomLeftName => TactileEditorTextures.BorderRadiusBottomLeft,
+                _ when currentSelected == bottomRightName => TactileEditorTextures.BorderRadiusBottomRight,
+                _ => TactileEditorTextures.BorderRadiusNoneSelected
+            });
+
+            GUI.DrawTexture(rects[0], borderRadiusImage, ScaleMode.StretchToFill);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Runtime/Geometry/Rectangle.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tactile.UI
+{
+    /// <summary>
+    /// A rectangle that can have each of its corners curved. A number of corner vertices can be specified to increase
+    /// the resolution of the corners.
+    /// </summary>
+    [RequireComponent(typeof(CanvasRenderer))]
+    [AddComponentMenu("UI/Rectangle")]
+    [ExecuteInEditMode]
+    public class Rectangle : Graphic
+    {
+        [SerializeField, Min(2)] private int cornerVertices = 3;
+        [SerializeField] private CornerRadii corners;
+        [SerializeField] private bool useNormalizedUV = true;
+
+        public CornerRadii Corners
+        {
+            get => corners;
+            set
+            {
+                corners = value;
+                SetVerticesDirty();
+            }
+        }
+        
+        protected readonly static Dictionary<int, (Vector2[], int[])> CornerCache = new Dictionary<int, (Vector2[], int[])>();
+        protected static readonly (bool, bool) TopLeftCorner = (false, true);
+        protected static readonly (bool, bool) TopRightCorner = (true, true);
+        protected static readonly (bool, bool) BottomLeftCorner = (false, false);
+        protected static readonly (bool, bool) BottomRightCorner = (true, false);
+        
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+            
+            var (tl_l, tl_o, tl_r) = CreateCorner(vh, TopLeftCorner);
+            var (tr_l, tr_o, tr_r) = CreateCorner(vh, TopRightCorner);
+            var (bl_l, bl_o, bl_r) = CreateCorner(vh, BottomLeftCorner);
+            var (br_l, br_o, br_r) = CreateCorner(vh, BottomRightCorner);
+            
+            // Fill edges
+            StitchEdges(vh, tl_r, tr_l, tl_o, tr_o);
+            StitchEdges(vh, tr_o, tr_r, br_o, br_r);
+            StitchEdges(vh, bl_o, br_o, bl_r, br_l);
+            StitchEdges(vh, tl_l, tl_o, bl_l, bl_o);
+            
+            // Fill center
+            StitchEdges(vh, tl_o, tr_o, bl_o, br_o);
+        }
+
+        protected void StitchEdges(VertexHelper vh, int topLeft, int topRight, int bottomLeft, int bottomRight)
+        {
+            vh.AddTriangle(bottomLeft, topLeft, topRight);
+            vh.AddTriangle(bottomLeft, topRight, bottomRight);
+        }
+
+        protected UIVertex GetNonRoundedUICorner((bool, bool) corner)
+        {
+            UIVertex uiv = UIVertex.simpleVert;
+            uiv.color = color;
+            uiv.position = GetCorner(corner);
+            uiv.uv0 = useNormalizedUV ? new Vector4(corner.Item1 ? 0 : 1, corner.Item2 ? 0 : 1) : uiv.position;
+            
+            return uiv;
+        }
+        
+        protected Vector2 GetCorner((bool, bool) corner)
+        {
+            Rect rect = rectTransform.rect;
+            Vector2 pivot = Vector2.Scale(rectTransform.pivot, rect.size);
+            float rectWidth = corner.Item1 ? rect.width : 0;
+            float rectHeight = corner.Item2 ? rect.height : 0;
+            Vector2 origin = new Vector2(rectWidth, rectHeight) - pivot;
+            return origin;
+        }
+
+        protected (int, int, int) CreateCorner(VertexHelper vh, (bool, bool) corner)
+        {
+            float cornerSize = corners[corner];
+            if (cornerSize > 0)
+            {
+                Rect rect = rectTransform.rect;
+                float width = Mathf.Min(cornerSize, rect.width / 2f) * (corner.Item1 ? 1 : -1);
+                float height = Mathf.Min(cornerSize, rect.height / 2f) * (corner.Item2 ? 1 : -1);
+                Vector2 origin = GetCorner(corner) - new Vector2(width, height);
+                
+                return CreateCorner(vh, width, height, origin);
+            }
+
+            int vertexIndex = vh.currentVertCount;
+            vh.AddVert(GetNonRoundedUICorner(corner));
+            
+            return (vertexIndex, vertexIndex, vertexIndex);
+            
+        }
+
+        protected (int, int, int) CreateCorner(VertexHelper vh, float width, float height, Vector2 origin)
+        {
+            if (!CornerCache.ContainsKey(cornerVertices))
+            {
+                CornerCache.Add(cornerVertices, BuildCorner(cornerVertices));
+            }
+
+            var (verts, triangles) = CornerCache[cornerVertices];
+            Vector2 scaleVector = new Vector2(width, height);
+            int triangleOffset = vh.currentVertCount;
+            
+            // Add verts
+            for (int i = 0; i < verts.Length; i++)
+            {
+                UIVertex uiv = UIVertex.simpleVert;
+                uiv.position = Vector2.Scale(verts[i], scaleVector) + origin;
+                uiv.uv0 = useNormalizedUV ? verts[i] + origin : uiv.position;
+                uiv.color = color;
+                vh.AddVert(uiv);
+            }
+            
+            // Add triangles
+            bool heightFlip = height < 0;
+            bool widthFlip = width < 0;
+            bool needFlip = heightFlip != widthFlip;
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                int t1 = triangles[i] + triangleOffset;
+                int t2 = triangles[i + 1] + triangleOffset;
+                int t3 = triangles[i + 2] + triangleOffset;
+                if (needFlip)
+                {
+                    vh.AddTriangle(t1, t3, t2);    
+                }
+                else
+                {
+                    vh.AddTriangle(t1, t2, t3);
+                }
+            }
+
+
+            int leftVert = verts.Length - 1 + triangleOffset;
+            int originVert = triangleOffset;
+            int rightVert = triangleOffset + 1;
+
+            return (widthFlip ? rightVert : leftVert, originVert, widthFlip ? leftVert : rightVert);
+        }
+
+        protected static (Vector2[], int[]) BuildCorner(int numCornerVertices)
+        {
+            if (numCornerVertices < 2)
+            {
+                throw new ArgumentException("Cannot make a corner with less than two corner vertices.");
+            }
+            
+            int totalTriangleIndices = 3 * (numCornerVertices - 1);
+            int totalVerts = numCornerVertices + 1;
+            Vector2[] verts = new Vector2[totalVerts];
+            int[] triangles = new int[totalTriangleIndices];
+            const float halfPi = Mathf.PI / 2f;
+            verts[0] = Vector2.zero;
+            verts[1] = new Vector2(1f, 0f);
+            for (int i = 2; i <= numCornerVertices; i++)
+            {
+                float angle = ((float)(i - 1) / (numCornerVertices - 1)) * halfPi;
+                Vector2 v = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                verts[i] = v;
+                
+                // Create origin, line segment, prev line segment triangle.
+                int ti = 3 * (i - 2);
+                triangles[ti] = 0;
+                triangles[ti + 1] = i;
+                triangles[ti + 2] = i - 1;
+            }
+
+            return (verts, triangles);
+        }
+
+        [Serializable]
+        public struct CornerRadii
+        {
+            public float topLeft;
+            public float topRight;
+            public float bottomLeft;
+            public float bottomRight;
+
+            public float this[(bool, bool) corner] => GetCornerSize(corner);
+
+            public float GetCornerSize((bool, bool) corner)
+            {
+                return corner switch
+                {
+                    (false, false) => bottomLeft,
+                    (false, true) => topLeft,
+                    (true, false) => bottomRight,
+                    (true, true) => topRight,
+                };
+            }
+        }
+    }
+}
