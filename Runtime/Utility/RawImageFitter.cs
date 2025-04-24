e411443b2128eb9a8ef8a84a32ef1940fb9ab795
D.J. Figueroa <djfigs10@gmail.com>
2025-04-24 19:33:52 -0400

    fix image fitter

--- Runtime/Utility/RawImageFitter.cs
+++ Runtime/Utility/RawImageFitter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Tactile.UI.Utility
@@ -7,15 +8,33 @@ namespace Tactile.UI.Utility
     [RequireComponent(typeof(AspectRatioFitter))]
     public class RawImageFitter : MonoBehaviour
     {
+        [SerializeField] private FitMode imageFit;
+        
         private RawImage _rawImage;
         private AspectRatioFitter _fitter;
         private Vector2Int _lastDimensions;
         
+        public FitMode ImageFit
+        {
+            get => imageFit;
+            set => SetFitMode(value);
+        }
+        
         #region Unity Events
-
+        
         private void Awake()
         {
             _rawImage = GetComponent<RawImage>();
+            _fitter = GetComponent<AspectRatioFitter>();
+            SetFitMode(imageFit);
+        }
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                SetFitMode(imageFit);    
+            }
         }
 
         private void Update()
@@ -27,13 +46,33 @@ namespace Tactile.UI.Utility
             var currentSize = new Vector2Int(tex.width, tex.height);
             if (currentSize == _lastDimensions)
                 return;
-
             var ratio = (float)currentSize.x / currentSize.y;
             _fitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
             _fitter.aspectRatio = ratio;
             _lastDimensions = currentSize;
+            SetFitMode(imageFit);
         }
 
         #endregion
+
+        private void SetFitMode(FitMode newFitMode)
+        {
+            if (!_fitter)
+                return;
+            
+            imageFit = newFitMode;
+            _fitter.aspectMode = imageFit switch
+            {
+                FitMode.Fill => AspectRatioFitter.AspectMode.EnvelopeParent,
+                FitMode.Fit => AspectRatioFitter.AspectMode.FitInParent,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public enum FitMode
+        {
+            Fill,
+            Fit
+        }
     }
 }
