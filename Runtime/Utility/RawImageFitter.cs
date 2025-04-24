f477c6bac6acd983c0c4ab51019657cc34ba4ac7
D.J. Figueroa <djfigs10@gmail.com>
2025-04-24 19:21:49 -0400

    add raw image fitter

---
+++ Runtime/Utility/RawImageFitter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tactile.UI.Utility
+{
+    [RequireComponent(typeof(RawImage))]
+    [RequireComponent(typeof(RawImageFitter))]
+    public class RawImageFitter : MonoBehaviour
+    {
+        private RawImage _rawImage;
+        private AspectRatioFitter _fitter;
+        private Vector2Int _lastDimensions;
+        
+        #region Unity Events
+
+        private void Awake()
+        {
+            _rawImage = GetComponent<RawImage>();
+        }
+
+        private void Update()
+        {
+            if (!_rawImage || !_rawImage.texture)
+                return;
+
+            var tex = _rawImage.texture;
+            var currentSize = new Vector2Int(tex.width, tex.height);
+            if (currentSize == _lastDimensions)
+                return;
+
+            var ratio = (float)currentSize.x / currentSize.y;
+            _fitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+            _fitter.aspectRatio = ratio;
+            _lastDimensions = currentSize;
+        }
+
+        #endregion
+    }
+}
